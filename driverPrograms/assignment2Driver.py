08fc17d04b0c96d57d0981a29aee247376f16b46
Kunj's Arch <kp03@KunjPathak.localdomain>
2023-01-05 13:25:26 +0530

    intermediate commit for assignment 2

---
+++ driverPrograms/assignment2Driver.py
@@ -0,0 +1,8 @@
+from utilPrograms.assignment2Util import *
+print(totalLinesRdd(loadDataToRDD))
+print(totalWarnRdd(loadDataToRDD))
+print(totalRepoProcessed(loadDataToRDD))
+mostHttp(articulatedDf)
+failedRequest(articulatedDf)
+activeHourOfTheDay(articulatedDf)
+mostActiveRepo(articulatedDf)
